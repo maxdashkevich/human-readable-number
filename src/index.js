4d2655a8abd8967f19c7385c2d32f7f90da3edc7
maxim dashkevich <maximkacr7@mail.com>
2020-03-22 18:00:16 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function toReadable (number) {
-  
+  if (number === 0) return 'zero';
+  let numbers = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+      20: 'twenty',
+      30: 'thirty',
+      40: 'forty',
+      50: 'fifty',
+      60: 'sixty',
+      70: 'seventy',
+      80: 'eighty',
+      90: 'ninety'
+  }
+  let words = number.toString();
+  let hundreds = '', result = '';
+  if (words.length === 3) {
+      hundreds = numbers[words[0]] + ' hundred';
+      words = words.slice(1);
+  }
+
+  if(numbers[words]) {
+      result = hundreds + ' ' + numbers[words];
+  } else {
+      if (words == '00') {
+          return hundreds;
+      }
+      let tens = numbers[words[0]] ? numbers[words[0]*10] : '';
+      let unit = numbers[words[1]];
+    
+      result = (tens != '') ? (hundreds + ' ' + tens + ' ' + unit) : (hundreds + ' ' + unit);
+  }
+  return result.trim();
 }
